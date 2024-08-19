41c9149a05d223393bd9a7e01b3651152bcd0d1b
meredithchin <82967123+meredithchin@users.noreply.github.com>
2024-08-19 22:20:51 +0800

    Update plan.js

--- plan.js
+++ plan.js
@@ -1,16 +1,4 @@
-//Theme
-var icon = document.getElementById("icon");
 
-icon.onclick = function() {
-  document.body.classList.toggle("dark-theme");
-
-  if (document.body.classList.contains("dark-theme")) {
-    icon.src = "sun.png";
-  }
-  else {
-    icon.src = "moon.png";
-  }
-}
 
 document.addEventListener('DOMContentLoaded', loadSavedPlans);
 
